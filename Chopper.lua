29d14a38fa2e8ea317b8cc4495a202ace030d6eb
SchneeWelt <discordbots4learning@gmail.com>
2022-05-03 20:00:55 +0200

    Update Chopper.lua

--- Chopper.lua
+++ Chopper.lua
@@ -1,7 +1,7 @@
---Diese Funktion lässt die Turtel einen Baum fällen.
+--Diese Funktion laesst die Turtel einen Baum faellen.
 --Es wird erwartet, dass sich die Turtel vor dem Aufruf
 --der Funktion ein Block über dem Boden und ein Block
---vor dem zufällenden Baum befindet. Ist das Fällen des Baumes
+--vor dem zufaellenden Baum befindet. Ist das Faellen des Baumes
 --abgeschlossen, befindet sich die Turtel wieder auf
 --ihrer startposition
 local function choppTree()
@@ -11,15 +11,15 @@ local function choppTree()
     turtle.digDown()
     turtle.placeDown()
 
-    start=1
-    fällhöhe=12
+    local start = 1
+    local faellhoehe = 12
 
-    for i=start,fällhöhe do
+    for i=start,faellhoehe do
         turtle.digUp()
         turtle.up()
     end
 
-    for i=start,fällhöhe do
+    for i=start,faellhoehe do
         turtle.down()
     end
     
@@ -28,13 +28,13 @@ end
 
 --Es wird davon ausgegangen, dass ein Block vor und hinter der Turtel jeweils eine Bahn
 --befindlich ist. Diese Bahnen existieren nicht wirklich, dienen der Turtle aber dazu, genau
---die fällplätze anzusteuern.
+--die faellplaetze anzusteuern.
 --Diese Funktion bewegt die Turtel auf dieser Bahn. Es ist die laufweite, also
 --wie weit die Turtel nach links oder rechts laufen soll anzugeben, negative und
---positive werte sind entsprechend möglich. Des weiteren ist die bahn anzugeben.
+--positive werte sind entsprechend moeglich. Des weiteren ist die bahn anzugeben.
 --Die obere bahn hat die bahnId 0, die untere, die bahnId 1. Die Funktion ist nur
 --wirksahm, wenn die Turtel auf ihrer startposition steht.
-local function bahnpositionÄndern(bahnId, laufweite)
+local function bahnpositionAendern(bahnId, laufweite)
 
     --steuert die obere Bahn an
     if tonumber(bahnId)==0 then
@@ -42,14 +42,14 @@ local function bahnpositionÄndern(bahnId, laufweite)
         turtle.up()
         turtle.forward()
 
-        --lässt die turtel nach links laufen
+        --laesst die turtel nach links laufen
         if tonumber(laufweite)<=0 then
             turtle.turnLeft()
             for i=1,-1*laufweite do
                 turtle.forward()
             end
             turtle.turnRight()
-        else --lässt die Turtle nach rechts laufen
+        else --laesst die Turtle nach rechts laufen
             turtle.turnRight()
             for i=1,laufweite do
                 turtle.forward()
@@ -66,14 +66,14 @@ local function bahnpositionÄndern(bahnId, laufweite)
         turtle.turnLeft()
         turtle.forward()
 
-        --lässt die turtel nach links laufen
+        --laesst die turtel nach links laufen
         if tonumber(laufweite)<=0 then
            turtle.turnRight()
             for i=1,-1*laufweite do
                 turtle.forward()
             end
             turtle.turnLeftt()
-        else --lässt die Turtle nach rechts laufen
+        else --laesst die Turtle nach rechts laufen
             turtle.turnLeft()
             for i=1,laufweite do
                 turtle.forward()
@@ -85,20 +85,37 @@ end
 
 --bahnId beschreibt, ob die obere oder die Untere Bahn angesteuert werden soll
 local function choppA(bahnId)
-        bahnpositionÄndern(bahnId, -4)
+        bahnpositionAendern(bahnId, -4)
         choppTree()
-        turtle.turnLeft()
+
+        --obere Bahn
+        if tonumber(bahnId)==0 then
+            turtle.turnRight()
+        else
+            turtle.turnLeft()
+        end
+
         for i=1,4 do
             turtle.forward()
         end
+
+    --obere Bahn
+    if tonumber(bahnId)==0 then
+        turtle.turnRight()
+        turtle.forward()
+        turtle.turnRight()
+        turtle.turnRight()
+        turtle.down()
+    else --untere Bahn
         turtle.turnLeft()
         turtle.forward()
         turtle.down()
+    end
 end
 
 local function choppB(bahnId)
 
-    bahnpositionÄndern(bahnId, -1)
+    bahnpositionAendern(bahnId, -1)
     for i=1,5 do
         turtle.forward()
     end
@@ -108,16 +125,33 @@ local function choppB(bahnId)
     for i=1,5 do
         turtle.forward()
     end
-    turtle.turnRight()
+
+    --obere Bahn
+    if tonumber(bahnId)==0 then
+        turtle.turnLeft()
+    else
+        turtle.turnRight()
+    end
+
     turtle.forward()
-    turtle.turnLeft()
-    turtle.forward()
-    turtle.down()
+
+    --obere Bahn
+    if tonumber(bahnId)==0 then
+        turtle.turnRight()
+        turtle.forward()
+        turtle.turnRight()
+        turtle.turnRight()
+        turtle.down()
+    else --untere Bahn
+        turtle.turnLeft()
+        turtle.forward()
+        turtle.down()
+    end
 end
 
 local function choppC(bahnId)
 
-    bahnpositionÄndern(bahnId, 1)
+    bahnpositionAendern(bahnId, 1)
     for i=1,10 do
         turtle.forward()
     end
@@ -127,16 +161,33 @@ local function choppC(bahnId)
     for i=1,10 do
         turtle.forward()
     end
-    turtle.turnLeft()
+
+    --obere Bahn
+    if tonumber(bahnId)==0 then
+        turtle.turnRight()
+    else
+        turtle.turnLeft()
+    end
+
     turtle.forward()
-    turtle.turnRight()
-    turtle.forward()
-    turtle.down()
+
+    --obere Bahn
+    if tonumber(bahnId)==0 then
+        turtle.turnLeft()
+        turtle.forward()
+        turtle.turnRight()
+        turtle.turnRight()
+        turtle.down()
+    else --untere Bahn
+        turtle.turnRight()
+        turtle.forward()
+        turtle.down()
+    end
 end
 
 local function choppD(bahnId)
 
-    bahnpositionÄndern(bahnId, 4)
+    bahnpositionAendern(bahnId, 4)
     for i=1,5 do
         turtle.forward()
     end
@@ -146,30 +197,64 @@ local function choppD(bahnId)
     for i=1,5 do
         turtle.forward()
     end
-    turtle.turnLeft() --oder turnRight hängt von der bahnId ab...
-    for i=1,3 do 
+
+    --obere Bahn
+    if tonumber(bahnId)==0 then
+        turtle.turnRight()
+    else
+        turtle.turnLeft()
+    end
+
+    for i=1,3 do
         turtle.forward()
     end
-    turtle.turnRight()
-    turtle.forward()
-    turtle.down()
+
+    --obere Bahn
+    if tonumber(bahnId)==0 then
+        turtle.turnLeft()
+        turtle.forward()
+        turtle.turnRight()
+        turtle.turnRight()
+        turtle.down()
+    else --untere Bahn
+        turtle.turnRight()
+        turtle.forward()
+        turtle.down()
+    end
 end
 
 local function choppE(bahnId)
 
-    bahnpositionÄndern(bahnId, 6)
+    bahnpositionAendern(bahnId, 6)
     choppTree()
-    turtle.turnRight()
+
+    -- obere Bahn
+    if tonumber(bahnId)==0 then
+        turtle.turnLeft()
+    else
+        turtle.turnRight()
+    end
+
     for i=1,6 do
         turtle.forward()
     end
-    turtle.turnRight()
-    turtle.forward()
-    turtle.down()
+
+    --obere Bahn
+    if tonumber(bahnId)==0 then
+        turtle.turnLeft()
+        turtle.forward()
+        turtle.turnRight()
+        turtle.turnRight()
+        turtle.down()
+    else --untere Bahn
+        turtle.turnRight()
+        turtle.forward()
+        turtle.down()
+    end
 end
 
-choppA()
-chopB()
-chopC()
-choppD()
-choppE()
+choppA(1)
+chopB(1)
+chopC(1)
+choppD(1)
+choppE(1)
